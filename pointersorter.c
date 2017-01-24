579ef2fce3a02eeb938773e8dc50c619ed1fa262
Ryan Morey <ryantmorey@gmail.com>
2017-01-24 11:05:56 -0500

    Fix bugs add printing

--- pointersorter.c
+++ pointersorter.c
@@ -3,27 +3,32 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef struct Nodes {
-    char* word;
+typedef struct Node{
+    // pointer to the first char of the word in the input string
+    char * word;
     struct Node* next;
 } Node;
 
-int isnull(char c){
-    return (c == '\0');
-}
 
-Node* insert(Node* root, Node* n){
-    if (root == NULL){
+// Recursively insert the node into the linked list
+Node* insert(Node* head, Node* n){
+    if (head == NULL){
         return n;
     }
-    if (strcmp(root->word,n->word) > 0){
-        n->next = root;
+    if (strcmp(head->word,n->word) > 0){
+        n->next = head;
         return n;
     }
-    root->next = insert(root->next,n);
-    return root;
+    head->next = insert(head->next,n);
+    return head;
 }
 
+void printlist(Node* head){
+    while(head != NULL){
+        printf("%s\n",head->word);
+        head = head->next;
+    }
+}
 
 int main(int argc, char * argv[]) {
     if (argc < 2) {
@@ -34,35 +39,40 @@ int main(int argc, char * argv[]) {
         printf("Too many arguments, needs exactly one\n");
         exit(1);
     }
-    char * input = argv[1];
-    int wordlen;
+    char* input = argv[1];
+    printf("%s\n",input);
     int i;
+    // Get to the first word
     for (i=0;!isalpha(input[i]);i++){
-        if isnull(input[i]){
+        if (input[i] == '\0'){
             exit(0);
         }
     }
-    Node* root = NULL;
-    while (!isnull(input[i])){
+    // Declare a Node pointer which will point to the head of the linked list
+    Node* head = NULL; 
+    int wordlen;
+    while (input[i] != '\0'){
         wordlen = 0;
         while (isalpha(input[i])){
             i++;
             wordlen++;
         }
-        Node n;
-        n->word = (char *)malloc(wordlen+1);
-        memcpy(n->word,&input[i-wordlen],wordlen);
+        Node* n = malloc(sizeof(Node));
+        n->word = input + i - wordlen;
         n->word[wordlen] = '\0';
-        root = insert(root,n)
-        if (isnull(input[i])){
-            exit(0);
+        i++;
+        head = insert(head,n);
+        if (input[i] == '\0'){
+            break;
         }
+        // Get to the next word
         while (!isalpha(input[i])){
             i++;
-            if (isnull(input[i])){
-                exit(0);
+            if (input[i] == '\0'){
+                break;
             }
         }
     }
+    printlist(head);
 	return 0;
 }
