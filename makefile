d3892ec5534ee5936d85741c8755112e29fd60f7
Ryan Morey <ryantmorey@gmail.com>
2017-01-23 21:29:13 -0500

    Start doing string stuff

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 all: pointersorter.c
-	gcc -o pointersorter pointersorter.c
+	gcc -o bin/pointersorter pointersorter.c
+
+tar: pointersorter.c testcases.txt
+	cp pointersorter.c testcases.txt Asst0
+	tar -czf Asst0.tgz Asst0
--- pointersorter.c
+++ pointersorter.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-	printf("Hello world!\n");
+int main(int argc, char * argv[]) {
+    if (argc < 2) {
+        printf("Needs one argument, none given\n");
+        exit(1);
+    }
+    if (argc > 2){
+        printf("Too many arguments, needs exactly one\n");
+        exit(1);
+    }
+    printf("The string is \"%s\"\n", argv[1]);
 	return 0;
 }
